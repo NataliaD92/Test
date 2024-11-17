9f28bd1b259b9dfa549d690faaca1a08cdfc4712
NataliaD92 <natalia.d.051992@gmail.com>
2024-11-17 08:27:36 +0100

    new file added

---
+++ main.py
@@ -0,0 +1,67 @@
+# a = 5
+# print(f"Wartość a to: {a}")
+# print(type(a))
+
+# b = 5.0
+# print(f"Wartość b to: {b}")
+# print(type(b))
+
+# -------------------
+
+# a = 1.0
+# print(isinstance(a, float))
+
+# -------------------
+
+# user_input = input("Proszę wprowadzić jakieś dane: ")
+# print("Wprowadziłeś:", user_input)
+
+# -------------------
+
+# name = "Alice"
+# age = 30
+# print("Name:", name, "Age:", age)
+# print(f"Name: {name} Age: {age}")
+
+# -------------------
+
+# print("a", "b", "c", sep="")
+# print("a", "b", "c")
+
+# # -------------------
+
+# print("Hello", end=" ")
+# print("world!")
+
+# -------------------
+
+a = 3
+b = 10
+
+# Dodawanie
+result_add = a + b
+print("Dodawanie:", result_add)
+
+# Odejmowanie
+result_subtract = a- b
+print("Odejmowanie:", result_subtract)
+
+# Mnożenie
+result_multiply = a * b
+print("Mnożenie:", result_multiply)
+
+# Dzielenie
+result_divide = a / b
+print("Dzielenie:", result_divide)
+
+# Modulo
+result_modulo = a % b
+print("Moduł:", result_modulo)
+
+# Potęgowanie
+result_power = a ** b
+print("Potęgowanie:", result_power)
+
+# Dzielenie całkowite
+result_floor_divide = a // b
+print("Dzielenie całkowite:", result_floor_divide)
